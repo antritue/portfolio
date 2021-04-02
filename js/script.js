03db5d1ec797349485949e89aa4a31108d0469d7
An Tri Tue <antritue@gmail.com>
2021-04-02 22:52:41 +0700

    commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Get the modal
+var modal = document.getElementsByClassName("modal");
+
+// Get the image that opens the modal
+var thumbnail = document.getElementsByClassName("thumbnail");
+
+// When the user clicks on the image, open the modal
+for (let i = 0; i < thumbnail.length; i++) {
+    thumbnail[i].onclick = function() {
+        modal[i].style.display = "block";
+    }
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    for (let i = 0; i < modal.length; i++)
+        if (event.target == modal[i]) {
+            modal[i].style.display = "none";
+        }
+}
